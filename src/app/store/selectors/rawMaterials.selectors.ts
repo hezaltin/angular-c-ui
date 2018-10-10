d583f9a97210d21d1f1793447c41042b19964500
AP\rajatmh <raja_t_michael_h@cat.com>
2018-10-10 11:33:17 +0530

    commit-with-the-chart-responsive-changes

--- src/app/store/selectors/rawMaterials.selectors.ts
+++ src/app/store/selectors/rawMaterials.selectors.ts
@@ -3,6 +3,6 @@ import * as fromFeature from '../reducers';
 import * as fromRawMaterials from '../reducers/rawMaterials.reducers';
 //import {Products} from '../../models/product.model';
 
-export const getRawMaterials = createSelector(fromFeature.getRawMaterialsState, (state:fromRawMaterials.RawMaterialsState)=>state);
+//export const getRawMaterials = createSelector(fromFeature.getRawMaterialsState, (state:fromRawMaterials.RawMaterialsState)=>state);
 
-export const getRawMaterialsEntites= createSelector(getRawMaterials,fromRawMaterials.getRawMaterialsEntites);
+export const getRawMaterialsEntites= createSelector(fromFeature.getRawMaterialsState,fromRawMaterials.getRawMaterialsEntites);
